3526568f41219aae426bdba32745ac1043ed6038
Kurt Kaiser <kurtbkaiser@gmail.com>
2020-03-20 12:59:11 -0400

    Add files via upload

---
+++ OnGroundScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGroundScript : MonoBehaviour
+{
+    GameObject Player;
+
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerScript>().onPlatform = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Player.GetComponent<PlayerScript>().onPlatform = false;
+    }
+}
---
+++ PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int moveSpeed = 7;
+    private Vector3 movement;
+    public float jumpHeight = 5f;
+    public bool onPlatform = true;
+
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position = transform.position +  movement * moveSpeed * Time.deltaTime;
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && onPlatform)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+}
